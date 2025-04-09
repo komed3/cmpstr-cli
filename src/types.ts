5a3567078e7edadda3a8a2321aa503f1597ebd1d
komed3 <pk97@outlook.de>
2025-04-09 16:53:50 +0200

    add measurePerf module for performance tracking

--- src/types.ts
+++ src/types.ts
@@ -1,12 +1,12 @@
-/**
- * interface ConfigOptions
- * @public
- */
-
 export interface ConfigOptions {
     algo?: string;
     flags?: string;
     options?: Record<string, any>;
     async?: boolean;
     verbose?: boolean;
+}
+
+export interface PerfResult {
+    time: number,
+    memory: number
 }
