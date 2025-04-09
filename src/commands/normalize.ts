15ce6cca069a2d84f3443c650b68d575b4aa6c00
komed3 <pk97@outlook.de>
2025-04-09 11:45:43 +0200

    implement normalize command for cmpstr-cli

---
+++ src/commands/normalize.ts
@@ -0,0 +1,43 @@
+/**
+ * Command `normalize` for `cmpstr-cli`
+ * 
+ * Normalizes a single string or a list of strings
+ * using the cmpstr normalize() method.
+ * 
+ * @author Paul Köhler
+ * @license MIT
+ */
+
+import { Command } from 'commander';
+import { CmpStrAsync } from 'cmpstr';
+
+import { loadConfig } from '../utils/loadConfig.js';
+import { mergeConfig } from '../utils/mergeConfig.js';
+import { validateConfig } from '../utils/validateConfig.js';
+import { readInput } from '../utils/readInput.js';
+import { readList } from '../utils/readList.js';
+
+export async function normalize (
+    input: string,
+    options : Record<string, any>,
+    cmd: Command
+) : Promise<void> {
+
+    const cfg = mergeConfig(
+        options,
+        await loadConfig( options?.config )
+    );
+
+    validateConfig( cfg );
+
+    const cmp = new CmpStrAsync ();
+
+    const data = options.list || false
+        ? await readList( input )
+        : await readInput( input );
+
+    const res = cfg.async
+        ? await cmp.normalizeAsync( data, cfg?.flags )
+        : cmp.normalize( data, cfg?.flags );
+
+}
