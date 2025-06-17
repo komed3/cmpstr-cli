722414729e12e83c6e8cbf863689cbdb6a09db36
komed3 <pk97@outlook.de>
2025-06-17 19:20:55 +0200

    implement output modes

--- src/utils/output.ts
+++ src/utils/output.ts
@@ -1,10 +1,34 @@
 'use strict';
 
-import { type Command } from 'commander';
 import type { Config } from './types.js';
+import { type Command } from 'commander';
+import * as fs from 'node:fs/promises';
+import chalk from 'chalk';
+import stripAnsi from 'strip-ansi';
 
-export async function output ( cfg: Config, cmd: Command, out: any ) : Promise<void> {
+export async function output ( cfg: Partial<Config>, cmd: Command, out: any ) : Promise<void> {
 
-    console.log( cfg, out );
+    if ( cfg.verbose ) {
+
+        out =
+            chalk.yellow( '[INPUT]' ) + '\n' +
+            'cmpstr ' + cmd.parent!.args.join( ' ' ) + '\n\n' +
+            chalk.yellow( '[RESULT]' ) + '\n' +
+            JSON.stringify( out, null, 2 ) + '\n\n' +
+            chalk.yellow( '[OPTIONS]' ) + '\n' +
+            JSON.stringify( cfg, null, 2 );
+
+    }
+
+    if ( cfg.output ) {
+
+        try { fs.writeFile( cfg.output, stripAnsi( out ), 'utf-8' ) }
+        catch ( err: any ) { throw new Error ( `Failed to write output: ${ err.message }` ) }
+
+        console.log( chalk.green( `Result written to <${ cfg.output }>` ) );
+
+    }
+
+    else console.log( out );
 
 }
