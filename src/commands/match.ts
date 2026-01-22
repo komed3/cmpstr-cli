37a7db6f442351f3d892831c63a016c97cbc156a
komed3 <pk97@outlook.de>
2026-01-22 16:35:54 +0100

    rework match command

--- src/commands/match.ts
+++ src/commands/match.ts
@@ -12,8 +12,9 @@
 
 'use strict';
 
-import { type Command } from 'commander';
 import { CmpStrAsync } from 'cmpstr';
+import { type Command } from 'commander';
+
 import { cfg } from '../utils/config.js';
 import { resolveInput, resolveListInput } from '../utils/input.js';
 import { output } from '../utils/output.js';
@@ -25,42 +26,31 @@ import { output } from '../utils/output.js';
  * @async
  * @param {string} a - The base input string or list (as string or file path).
  * @param {string} b - The string to compare against.
- * @param {Record<string, any>} [opt] - Additional match options.
+ * @param {Record< string, any >} [opt] - Additional match options.
  * @param {Command} cmd - The Commander command instance.
- * @returns {Promise<void>}
+ * @returns {Promise< void >}
  */
 export async function match (
     a: string, b: string,
-    opt: Record<string, any> = Object.create( null ),
+    opt: Record< string, any > = Object.create( null ),
     cmd: Command
-) : Promise<void> {
-
+) : Promise< void > {
     const config = await cfg( cmd, opt );
-
     const {
         async = false, verbose = false, list = false, delimiter = ',',
         metric = 'levenshtein', flags = '',
         threshold = 0, sort = 'desc', n = Infinity
     } = config;
 
+    const cmp = CmpStrAsync.create().setRaw( verbose ).setMetric( metric ).setFlags( flags );
     const A = list ? await resolveListInput( a, delimiter ) : await resolveInput( a );
     const B = await resolveInput( b );
 
-    const cmp = CmpStrAsync
-        .create()
-        .setRaw( verbose )
-        .setMetric( metric )
-        .setFlags( flags );
-
-    let res = async
-        ? await cmp.matchAsync( A, B, threshold )
-        : cmp.match( A, B, threshold );
-
+    let res = async ? await cmp.matchAsync( A, B, threshold ) : cmp.match( A, B, threshold );
     res = res.sort( ( a: any, b: any ) => verbose
         ? sort === 'asc' ? a.res - b.res : b.res - a.res
         : sort === 'asc' ? a.match - b.match : b.match - a.match
     ).slice( 0, n );
 
-    output( config, cmd, verbose ? JSON.stringify( res ) : res );
-
+    await output( config, cmd, verbose ? JSON.stringify( res ) : res );
 }
