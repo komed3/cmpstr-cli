6bc1d88bc8ad270ecda8df91153207b242a211d1
komed3 <pk97@outlook.de>
2026-01-22 16:40:41 +0100

    Update pairs.ts

--- src/commands/pairs.ts
+++ src/commands/pairs.ts
@@ -12,8 +12,9 @@
 
 'use strict';
 
-import { type Command } from 'commander';
 import { CmpStrAsync } from 'cmpstr';
+import { type Command } from 'commander';
+
 import { cfg } from '../utils/config.js';
 import { resolveListInput } from '../utils/input.js';
 import { output } from '../utils/output.js';
@@ -25,35 +26,24 @@ import { output } from '../utils/output.js';
  * @async
  * @param {string} a - The first list as a string or file path.
  * @param {string} b - The second list as a string or file path.
- * @param {Record<string, any>} [opt] - Additional options for comparison.
+ * @param {Record< string, any >} [opt] - Additional options for comparison.
  * @param {Command} cmd - The Commander command instance.
- * @returns {Promise<void>}
+ * @returns {Promise< void >}
  */
 export async function pairs (
     a: string, b: string,
-    opt: Record<string, any> = Object.create( null ),
+    opt: Record< string, any > = Object.create( null ),
     cmd: Command
-) : Promise<void> {
-
+) : Promise< void > {
     const config = await cfg( cmd, opt );
-
     const {
         async = false, verbose = false, delimiter = ',',
         metric = 'levenshtein', flags = ''
     } = config;
 
+    const cmp = CmpStrAsync.create().setRaw( verbose ).setMetric( metric ).setFlags( flags );
     const A = await resolveListInput( a, delimiter );
     const B = await resolveListInput( b, delimiter );
 
-    const cmp = CmpStrAsync
-        .create()
-        .setRaw( verbose )
-        .setMetric( metric )
-        .setFlags( flags );
-
-    output( config, cmd, async
-        ? await cmp.pairsAsync( A, B )
-        : cmp.pairs( A, B )
-    );
-
+    await output( config, cmd, async ? await cmp.pairsAsync( A, B ) : cmp.pairs( A, B ) );
 }
