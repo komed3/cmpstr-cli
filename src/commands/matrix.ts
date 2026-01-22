3b511e14f0320f201acdc42bf75f813b618df112
komed3 <pk97@outlook.de>
2026-01-22 16:39:03 +0100

    Update matrix.ts

--- src/commands/matrix.ts
+++ src/commands/matrix.ts
@@ -11,8 +11,9 @@
 
 'use strict';
 
-import { type Command } from 'commander';
 import { CmpStrAsync } from 'cmpstr';
+import { type Command } from 'commander';
+
 import { cfg } from '../utils/config.js';
 import { resolveListInput } from '../utils/input.js';
 import { output } from '../utils/output.js';
@@ -22,27 +23,19 @@ import { output } from '../utils/output.js';
  * 
  * @async
  * @param {string} input - The input list as a string or file path.
- * @param {Record<string, any>} [opt] - Additional matrix options.
+ * @param {Record< string, any >} [opt] - Additional matrix options.
  * @param {Command} cmd - The Commander command instance.
- * @returns {Promise<void>}
+ * @returns {Promise< void >}
  */
 export async function matrix (
     input: string,
-    opt: Record<string, any> = Object.create( null ),
+    opt: Record< string, any > = Object.create( null ),
     cmd: Command
-) : Promise<void> {
-
+) : Promise< void > {
     const config = await cfg( cmd, opt );
-
     const { async = false, delimiter = ',', metric = 'levenshtein', flags = '' } = config;
-
     const list = await resolveListInput( input, delimiter );
-
     const cmp = CmpStrAsync.create().setMetric( metric ).setFlags( flags );
 
-    output( config, cmd, async
-        ? await cmp.matrixAsync( list )
-        : cmp.matrix( list )
-    );
-
+    await output( config, cmd, async ? await cmp.matrixAsync( list ) : cmp.matrix( list ) );
 }
