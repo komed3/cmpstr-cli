d2eb43b10e437b596ab3eb15c01857acab2959ef
komed3 <pk97@outlook.de>
2026-01-22 16:57:01 +0100

    await file is writter + min fixes

--- src/utils/output.ts
+++ src/utils/output.ts
@@ -11,11 +11,13 @@
 
 'use strict';
 
-import type { Config } from './types.js';
-import { type Command } from 'commander';
-import * as fs from 'node:fs/promises';
+import { writeFile } from 'node:fs/promises';
+
 import chalk from 'chalk';
+import { type Command } from 'commander';
 import stripAnsi from 'strip-ansi';
+
+import type { Config } from './types.js';
 
 /**
  * Outputs the result to the console or writes it to a file.
@@ -23,37 +25,30 @@ import stripAnsi from 'strip-ansi';
  * Strips ANSI color codes when writing to a file.
  * 
  * @async
- * @param {Partial<Config>} cfg - The resolved configuration object.
+ * @param {Partial< Config >} cfg - The resolved configuration object.
  * @param {Command} cmd - The Commander command instance.
  * @param {any} out - The output to display or write.
- * @returns {Promise<void>}
  * @throws {Error} If writing to the file fails.
  */
-export async function output ( cfg: Partial<Config>, cmd: Command, out: any ) : Promise<void> {
-
+export async function output ( cfg: Partial< Config >, cmd: Command, out: any ) : Promise< void > {
     const { verbose = false, output = undefined } = cfg ?? {};
 
     // If verbose, prepend input and options information to the output
-    if ( verbose ) {
-
-        out =
-            chalk.yellow( '[INPUT]' ) + '\n' + 'cmpstr ' + cmd.parent!.args.join( ' ' ) + '\n\n' +
-            chalk.yellow( '[OPTIONS]' ) + '\n' + JSON.stringify( cfg, null, 2 ) + '\n\n' +
-            chalk.yellow( '[RESULT]' ) + '\n' + out;
-
-    }
+    if ( verbose ) out =
+        chalk.yellow( '[INPUT]' ) + '\n' + 'cmpstr ' + cmd.parent!.args.join( ' ' ) + '\n\n' +
+        chalk.yellow( '[OPTIONS]' ) + '\n' + JSON.stringify( cfg, null, 2 ) + '\n\n' +
+        chalk.yellow( '[RESULT]' ) + '\n' + out;
 
     if ( output ) {
-
         // Write output to file, stripping ANSI color codes
-        try { fs.writeFile( output, stripAnsi( out ), 'utf-8' ) }
-        catch ( err: any ) { throw new Error ( `Failed to write output: ${ err.message }` ) }
+        try { await writeFile( output, stripAnsi( out ), 'utf-8' ) }
+        catch ( err ) { throw new Error (
+            `Failed to write output: ${ ( err as unknown as Error ).message }`, { cause: err }
+        ) }
 
         console.log( chalk.green( `Result written to <${ output }>` ) );
-
     }
 
     // Output to console (with colors, if present)
     else console.log( out );
-
 }
